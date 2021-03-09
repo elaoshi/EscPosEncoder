e46324922ebb116ff7f31dd023ce31756abb4036
ericliu <39144388@qq.com>
2021-03-09 22:55:35 +1100

    Update esc-pos-encoder.js

--- src/esc-pos-encoder.js
+++ src/esc-pos-encoder.js
@@ -654,7 +654,10 @@ class EscPosEncoder {
    */
   drawTextLeftAndRight(left, right, nCharScale) {
     const width = this._line_byte_size;
-    const spaceNum = parseInt((width - left.length - right.length) / nCharScale);
+    let spaceNum = parseInt((width - left.length - right.length) / nCharScale);
+    if(spaceNum < 0){
+      spaceNum = 0;
+    }
     console.log('space is', spaceNum);
     const text = left + Array(spaceNum + 1).join(' ') + right;
     this.text(text);
